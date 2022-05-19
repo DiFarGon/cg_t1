f25ce9a0bf3059fb251f7ab79f7d49c6381afbc6
DiFarGon <diogo.faria.goncalves@tecnico.ulisboa.pt>
2022-05-19 16:07:29 +0100

    improved stuff

--- js/mesa.js
+++ js/mesa.js
@@ -1,12 +1,12 @@
 /*global THREE, requestAnimationFrame, console*/
 
+// grupo 23
+
 var camera, scene, renderer;
 
 var geometry, material, mesh;
 
-var icosahedron, box, train, tube, sphere, donut, compositeBoxes;
-
-var cube, cone, height_cone, width_boxLine, height_boxLine;
+var icosahedron, box, train, tube, sphere, donut, compositeBoxes, doubleCones;
 
 var grandfather, father, son;
 
@@ -16,111 +16,101 @@ var wireframe = false;
 var camAngle = 1, camChanged = true;
 var moveX = 0, moveY = 0, moveZ = 0;
 
-function addObject(x, y, obj) {
-    obj.position.x = x;
-    obj.position.y = y;
-  }
+function createTetrahedron(x, y, z, color) {
+    'use strict';
 
-  function createMaterial() {
+    var tetrahedron = new THREE.Object3D();
+
+    const radius = 15;
+
+    const geometry = new THREE.TetrahedronGeometry(radius);
     const material = new THREE.MeshBasicMaterial({
-      side: THREE.DoubleSide,
+        color: color,
+        wireframe: wireframe
     });
+    const mesh = new THREE.Mesh(geometry, material);
 
-    const hue = Math.random();
-    const saturation = 1;
-    const luminance = .5;
-    material.color.setHSL(hue, saturation, luminance);
+    tetrahedron.add(mesh);
+    
+    tetrahedron.position.set(x, y, z);
+    
+    scene.add(tetrahedron);
+}
 
-    return material;
-  }
+function createOctahedron(x, y, z, color) {
+    'use strict';
 
-  function addSolidGeometry(x, y, geometry) {
-    const mesh = new THREE.Mesh(geometry, createMaterial());
-    addObject(x, y, mesh);
+    var octahedron = new THREE.Object3D();
+
+    const radius = 20;
+
+    const geometry = new THREE.OctahedronGeometry(radius);
+    const material = new THREE.MeshBasicMaterial({
+        color: color,
+        wireframe: wireframe
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+
+    octahedron.add(mesh);
+
+    octahedron.position.set(x, y, z);
+
+    scene.add(octahedron);
+}
+
+function createCone(x, y, z, radius, height, color) {
+    'use strict';
+
+    const radialSegments = 20;
+    const heightSegments = 10;
+
+    const geometry = new THREE.ConeGeometry(radius, height, radialSegments, heightSegments);
+    const material = new THREE.MeshBasicMaterial({
+        color: color,
+        wireframe: wireframe
+    })
+    const mesh = new THREE.Mesh(geometry, material);
+
+    mesh.position.set(x, y, z);
+
     return mesh;
-  }
+}
 
-  function addLineGeometry(x, y, geometry) {
-    const material = new THREE.LineBasicMaterial({color: 0x000000});
-    const mesh = new THREE.LineSegments(geometry, material);
-    addObject(x, y, mesh);
-    return mesh;
-  }
+function createConeWithRotation(obj, x, y, z, radius, height, color, rotation) {
+    'use strict';
 
-  function addBoxLine(x, y){
-    width_boxLine = 30;
-    height_boxLine = 30;
-    const depth = 15;
-    const thresholdAngle = 15;
-    cube = addLineGeometry(x, y, new THREE.EdgesGeometry(
-        new THREE.BoxGeometry(width_boxLine, height_boxLine, depth),
-        thresholdAngle));
-    scene.add(cube);
- }
- function addCircle(){
-    const radius_circle = 5;
-    const segments_circle = 24;
-    const circle = addSolidGeometry(0, 0, new THREE.CircleGeometry(radius_circle, segments_circle));
-    cube.add(circle);
- }
- function addOctahedr(){
-    const radius_octahedr = 5;
-    const octahedr = addSolidGeometry(-width_boxLine/2, height_boxLine/2, new THREE.OctahedronGeometry(radius_octahedr));
-    cube.add(octahedr);
- }
- function addTerahedr(){
-    const radius_tetrahedr = 5;
-    const tetrahedr = addSolidGeometry(width_boxLine/2, height_boxLine/2, new THREE.TetrahedronGeometry(radius_tetrahedr));
-    cube.add(tetrahedr);
- }
- function addBoxWire(){
-    const width_box= 5;
-    const height_box = 5;
-    const depth_box = 15;
-    const box = addLineGeometry(-width_boxLine/2, -height_boxLine/2, new THREE.WireframeGeometry(new THREE.BoxGeometry(width_box, height_box, depth_box)));
-    cube.add(box);
- }
- function addBox(){
-    const width_box1 = 4;
-    const height_box1 = 4;
-    const depth_box1 = 6;
-    const box1 = addSolidGeometry(-width_boxLine/2, -height_boxLine/2, new THREE.BoxGeometry(width_box1, height_box1, depth_box1));
-    cube.add(box1);
-  }
-  function addCone(){
-    const radius_cone = 5;
-    height_cone = 5;
-    const segments_cone = 16;
-    cone = addSolidGeometry(width_boxLine/2, -height_boxLine/2, new THREE.ConeGeometry(radius_cone, height_cone, segments_cone)); 
-    cube.add(cone);
-  }
-  function addInevrtedCone(){
-    const radius_cone1 = 5;
-    const height_cone1 = 5;
-    const segments_cone1 = 16;
-    const cone1 = addSolidGeometry(0, -height_cone, new THREE.ConeGeometry(radius_cone1, height_cone1, segments_cone1));
-    cone.add(cone1);
-    cone1.rotateZ(Math.PI);
-  }
-  function addAll(x,y){
-    addBoxLine(x,y);
-    addCircle();
-    addOctahedr();
-    addTerahedr();
-    addBoxWire();
-    addBox();
-    addCone();
-    addInevrtedCone();
-  }
+    const mesh = createCone(x, y, z, radius, height, color);
+    mesh.scale.y = rotation;
 
-function addTrainMainWheel(obj, x, y, z) {
+    obj.add(mesh);
+}
+
+function createDoubleCones(x, y, z) {
+    'use strict';
+
+    doubleCones = new THREE.Object3D();
+    doubleCones.userData = {
+        spinning: true,
+        velocity: 0.05,
+    };
+
+    const radius = 30;
+    const height = 50;
+
+    createConeWithRotation(doubleCones, x, (y + (height / 2)), z, radius, height, 0xA26769, 1);
+    createConeWithRotation(doubleCones, x, (y - (height / 2)), z, radius, height, 0xD5B9B2, -1);
+
+    scene.add(doubleCones);
+}
+
+function addTrainMainWheel(obj, x, y, z, color) {
     'use strict';
     const radiusTop = 8;  // radiusTop
     const radiusBottom = 8;  // radiusBottom
     const height = 17;  // height
     const radialSegments = 12;  // radialSegments
     
-    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: wireframe });
+    material = new THREE.MeshBasicMaterial({ color: color, wireframe: wireframe });
     geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
     geometry.rotateZ(-Math.PI * 0.5);
 
@@ -129,17 +119,16 @@ function addTrainMainWheel(obj, x, y, z) {
     obj.add(mesh);
 }
 
-function addTrainCabin(obj, x, y, z) {
+function addTrainCabin(obj, x, y, z, color) {
     'use strict';
-    material = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: wireframe });
+    material = new THREE.MeshBasicMaterial({ color: color, wireframe: wireframe });
     geometry = new THREE.CubeGeometry(15, 20, 20);
     mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(x, y, z);
     obj.add(mesh);
 }
 
-function addWheel(obj, x, y, z, color) {
-    
+function addWheel(obj, x, y, z, color) { 
     'use strict';
 
     const radiusTop = 4;  // radiusTop
@@ -151,36 +140,32 @@ function addWheel(obj, x, y, z, color) {
     geometry.rotateZ(-Math.PI * 0.5);
     mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(x, y - 3, z);
-    obj.add(mesh);
-    
+    obj.add(mesh);  
 }
 
-function addTrainBody(obj, x, y, z) {
-    
+function addTrainBody(obj, x, y, z, color) {
     'use strict';
 
     const radiusTop = 8;  // radiusTop
     const radiusBottom = 8;  // radiusBottom
     const height = 32;  // height
     const radialSegments = 12;  // radialSegments
-    material = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: wireframe });
+    material = new THREE.MeshBasicMaterial({ color: color, wireframe: wireframe });
     geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
     geometry.rotateX(-Math.PI * 0.5);
     mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(x, y - 3, z);
     obj.add(mesh);
-    
 }
 
-function addTrainChimney(obj, x, y, z) {
-
+function addTrainChimney(obj, x, y, z, color) {
     'use strict';
 
     const radiusTop = 2;  // radiusTop
     const radiusBottom = 1;  // radiusBottom
     const height = 3;  // height
     const radialSegments = 8;  // radialSegments
-    material = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: wireframe });
+    material = new THREE.MeshBasicMaterial({ color: color, wireframe: wireframe });
     geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
     mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(x, y - 3, z);
@@ -193,13 +178,13 @@ function createTrain(x, y, z) {
     
     train = new THREE.Object3D();
     
-    addTrainCabin(train, 0, 0, 0);
-    addTrainMainWheel(train, 0, -2, 0);
-    addWheel(train, 0, -5, 17, 0x00ff00);
-    addWheel(train, 0, -5, 27, 0xff0000);
-    addWheel(train, 0, -5, 37, 0x0ff000);
-    addTrainBody(train, 0, 0, 25);
-    addTrainChimney(train, 0, 10, 37);
+    addTrainCabin(train, 0, 0, 0, 0x582C4D);
+    addTrainMainWheel(train, 0, -2, 0, 0xA26769);
+    addWheel(train, 0, -5, 17, 0xD5B9B2);
+    addWheel(train, 0, -5, 37, 0xD5B9B2);
+    addWheel(train, 0, -5, 27, 0xD5B9B2);
+    addTrainBody(train, 0, 0, 25, 0x582C4D);
+    addTrainChimney(train, 0, 10, 37, 0x582C4D);
     
     scene.add(train);
     
@@ -219,9 +204,9 @@ function createDonut(x, y, z, color) {
         velocity: 0.1
     }
     
-    const radius = 13;
-    const tube = 4;
-    const radialSegments = 500;
+    const radius = 52;
+    const tube = 16;
+    const radialSegments = 100;
     const tubularSegments = 50;
     
     const geometry = new THREE.TorusGeometry(radius, tube, radialSegments, tubularSegments);
@@ -246,7 +231,7 @@ function createSphere(x, y, z, color) {
         velocity: 0.1
     }
     
-    const radius = 8;
+    const radius = 32;
     const widthSegments = 50;
     const heightSegments = 50;
     
@@ -264,8 +249,8 @@ function createSphere(x, y, z, color) {
 }
 
 function createCompositeDonutSphere(x, y, z) {
-    createSphere(x, y, z, 0x000000);
-    createDonut(x, y, z, 0xff0000);
+    createSphere(x, y, z, 0xECE2D0);
+    createDonut(x, y, z, 0xA26769);
 }
 
 function createBox(obj, x, y, z, size, color) {
@@ -282,7 +267,7 @@ function createBox(obj, x, y, z, size, color) {
     obj.add(mesh);
 }
 
-function createCompositeBoxes(obj, x, y, z) {
+function createCompositeBoxes(x, y, z) {
     const box1 = 4;
     const box2 = 5;
     const box3 = 6;
@@ -295,30 +280,30 @@ function createCompositeBoxes(obj, x, y, z) {
               (y + (box1 / 2)),
               (z + (box1 / 2)),
               box1,
-              0x60492c);
+              0xECE2D0);
     createBox(compositeBoxes,
               (x + (box2 / 2)),
               (y + (box2 /2)),
               (z - (box2 / 2)),
               box2,
-              0xf9ebe0);
+              0x582C4D);
     createBox(compositeBoxes,
               (x - (box3 / 2)),
               (y - (box3 / 2)),
               (z - (box3 / 2)),
               box3,
-              0x208aae);
+              0xA26769);
     createBox(compositeBoxes,
               (x + (box4 / 2)),
               (y - (box4 / 2)),
               (z + (box4 / 2)),
               box4,
-              0x0d2149);
+              0xD5B9B2);
     
     return compositeBoxes;
 }
 
-function createIcosahedron(x, y, z) {
+function createIcosahedron(x, y, z, color) {
     'use strict';
 
     const radius = 10;
@@ -326,7 +311,7 @@ function createIcosahedron(x, y, z) {
     icosahedron = new THREE.Object3D();
 
     material = new THREE.MeshBasicMaterial({
-        color: 0x3d3a4b,
+        color: color,
         wireframe: wireframe,
     });
     geometry = new THREE.IcosahedronGeometry(radius);
@@ -338,7 +323,7 @@ function createIcosahedron(x, y, z) {
     return icosahedron;
 }
 
-function createTube(obj, x, y, z, color) {
+function createTube(x, y, z, color) {
     'use strict';
 
     tube = new THREE.Object3D();
@@ -347,12 +332,16 @@ function createTube(obj, x, y, z, color) {
     const radius = 0.5;
 
     const points = [
-        new THREE.Vector3(x - 10, y - 2, z),
+        new THREE.Vector3(x - 8, y + 20, z),
+        new THREE.Vector3(x - 10, y + 15, z - 3),
+        new THREE.Vector3(x - 2, y + 10, z + 2),
         new THREE.Vector3(x - 4, y + 2, z),
-        new THREE.Vector3(x, y - 5, z),
-        new THREE.Vector3(x + 4, y + 3, z),
-        new THREE.Vector3(x + 7, y - 10, z),
-        new THREE.Vector3(x + 10, y, z)
+        new THREE.Vector3(x, y - 5, z + 5),
+        new THREE.Vector3(x + 4, y + 3, z + 1),
+        new THREE.Vector3(x + 7, y - 10, z - 7),
+        new THREE.Vector3(x + 10, y, z - 4),
+        new THREE.Vector3(x + 15, y + 10, z),
+        new THREE.Vector3(x + 17, y - 10, z),
     ];
 
     var path = new THREE.CatmullRomCurve3(points);
@@ -373,9 +362,9 @@ function createTube(obj, x, y, z, color) {
 function createArticulatedObject(x, y, z) {
     'use strict';
 
-    grandfather = createIcosahedron(x, y, z);
-    father = createTube(grandfather, x - 10, y + 10, z, 0x000000);
-    son = createCompositeBoxes(father, x - 20, y + 40, z);
+    grandfather = createIcosahedron(x, y, z, 0x582C4D);
+    father = createTube(-10, 10, 10, 0xA26769);
+    son = createCompositeBoxes(-25, 40, 20);
     
     father.add(son)
     grandfather.add(father);
@@ -387,21 +376,18 @@ function createScene() {
     'use strict';
     
     scene = new THREE.Scene();
-
-    scene.add(new THREE.AxisHelper(10));
     
-    createTrain(40, 25, 0);
-    createCompositeDonutSphere(-50, -30, 0);
-    addAll(60, -30);
+    createTrain(40, 45, -60);
+    createCompositeDonutSphere(-100, -20, -80);
+    createDoubleCones(100, -40, -50);
+    createTetrahedron(5, -40, 80, 0xECE2D0);
+    createOctahedron(100, 70, 80, 0x582C4D);
     createArticulatedObject(0, 0, 0);
 }
 
 function createCamera() {
     'use strict';
-    camera = new THREE.PerspectiveCamera(70,
-                                         window.innerWidth / window.innerHeight,
-                                         1,
-                                         1000);
+    camera = new THREE.OrthographicCamera( -220, 230, 150, -150, 1, 1000 );
 }
 
 function onKeyDown(e) {
@@ -442,10 +428,10 @@ function onKeyDown(e) {
             moveX = 1
             break;
         case 67: // c
-            moveZ = -1
+            moveZ = 1
             break;
         case 68: // d
-            moveZ = 1
+            moveZ = -1
             break;
     }
 }
@@ -515,7 +501,7 @@ function init() {
         antialias: true
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0xe3dfff);
+    renderer.setClearColor(0xBFB5AF);
     document.body.appendChild(renderer.domElement);
    
     createScene();
@@ -537,6 +523,10 @@ function update() {
     if (sphere.userData.spinning) {
         sphere.rotateZ(sphere.userData.velocity);
     }
+
+    // if (doubleCones.userData.spinning) {
+    //     doubleCones.rotation.z += doubleCones.userData.velocity;
+    // }
 
     // rotation commands
     if (primaryRotationVelocity) {
@@ -570,17 +560,17 @@ function update() {
             case 1: // front
                 camera.position.x = 0;
                 camera.position.y = 0;
-                camera.position.z = 100;
+                camera.position.z = 200;
                 camera.lookAt(scene.position);
                 break;
             case 2: // top
                 camera.position.x = 0;
-                camera.position.y = 100;
+                camera.position.y = 200;
                 camera.position.z = 0;
                 camera.lookAt(scene.position);
                 break;
             case 3: // side
-                camera.position.x = 100;
+                camera.position.x = 200;
                 camera.position.y = 0;
                 camera.position.z = 0;
                 camera.lookAt(scene.position);
